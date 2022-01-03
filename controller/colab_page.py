0ed2442f784b2e8a80a5a7aeba6e0d5669935b7f
Gusrrocha <95317020+Gusrrocha@users.noreply.github.com>
2022-01-03 20:48:24 -0300

    Colab page

---
+++ controller/colab_page.py
@@ -0,0 +1,5 @@
+from qt_core import *
+class ColabPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('view/colab.ui', self)
--- controller/main_window.py
+++ controller/main_window.py
@@ -1,5 +1,13 @@
 from qt_core import *
-class MainWindow():
+from controller.colab_page import ColabPage
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('view/main_window.ui', self)
+
+
+        self.colab_btn.clicked.connect(self.show_colab)
+
+    def show_colab(self):
+        self.tabela.insertWidget(1, ColabPage())
+        self.tabela.setCurrentIndex(1)
